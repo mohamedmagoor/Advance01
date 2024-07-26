7e17e495cd516cbce3fe0221e6a02fbd52073e58
Mohamed Magoor <mohamedmagoor6@gmail.com>
2024-07-27 00:21:59 +0300

    Add files via upload

---
+++ Advance01/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Advance01
+{
+    internal class Program
+    {
+        static void OptimizedBubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                    }
+                }
+
+            }
+        }
+
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            ///1
+            int[] arr = { 5, 2, 9, 1, 7, 6 };
+            Console.WriteLine("the array before sorting =>...");
+            PrintArray(arr);
+            OptimizedBubbleSort(arr);
+            Console.WriteLine(" array after  =>  ");
+            PrintArray(arr);
+
+
+            //last section implement
+
+            var intRange = new Range<int>(1, 10);
+            Console.WriteLine("Int Range:");
+            Console.WriteLine($"Contains 5: {intRange.Contains(5)}"); 
+            Console.WriteLine($"Contains 15: {intRange.Contains(15)}"); 
+        }
+
+
+    }
+}
---
+++ Advance01/Range.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance01
+{
+    internal class Range<T> where T : IComparable<T>
+    {
+
+        public T Minimum { get; }
+        public T Maximum { get; }
+
+
+        public Range(T minimum, T maximum)
+        {
+            if (minimum.CompareTo(maximum) > 0)
+            {
+                throw new ArgumentException("Minimum should be less than or equal to Maximum.");
+            }
+            Minimum = minimum;
+            Maximum = maximum;
+        }
+        public bool Contains(T value)
+        {
+            return (Minimum.CompareTo(value) <= 0) && (Maximum.CompareTo(value) >= 0);
+        }
+
+    }
+}
